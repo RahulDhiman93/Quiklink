46e929b0d0eee0e62fb021af30ed96494e55a689
Rahul Dhiman <rsdm34@gmail.com>
2024-01-23 12:04:20 -0800

    Onboarding base code added

--- cmd/routes.go
+++ cmd/routes.go
@@ -3,15 +3,34 @@ package main
 import (
 	"Quiklink_BE/internal/handlers"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	gh "github.com/gorilla/handlers"
 	"net/http"
 )
 
 func routes() http.Handler {
 	mux := chi.NewRouter()
 
+	mux.Use(middleware.Logger)
+	mux.Use(middleware.Recoverer)
+
+	corsHandler := gh.CORS(
+		gh.AllowedOrigins([]string{"*"}),            // Allow all origins
+		gh.AllowedMethods([]string{"GET", "POST"}),  // Allow only specified methods
+		gh.AllowedHeaders([]string{"Content-Type"}), // Allow only specified headers
+	)
+
+	mux.Use(corsHandler)
+
 	mux.Get("/", handlers.Repo.Home)
 	mux.Post("/shorten", handlers.Repo.ShortenURL)
 	mux.Get("/{shortKey:[a-zA-Z0-9]+}", handlers.Repo.Redirect)
+
+	mux.Route("/auth", func(mux chi.Router) {
+		mux.Post("/login", handlers.Repo.LoginUser)
+		mux.Post("/register", handlers.Repo.RegisterUser)
+		mux.Get("/accessTokenLogin/{access_token}", handlers.Repo.AccessTokenLogin)
+	})
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
--- internal/models/models.go
+++ internal/models/models.go
@@ -11,3 +11,22 @@ type TemplateData struct {
 	Warning   string
 	Error     string
 }
+
+type AuthRequestBody struct {
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Phone     string `json:"phone"`
+}
+
+type User struct {
+	Id          int    `json:"id"`
+	AccessToken string `json:"access_token"`
+	Email       string `json:"email"`
+	Password    string `json:"password"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Phone       string `json:"phone"`
+	AccessLevel int    `json:"access_level"`
+}
