9b71ffbb3344f4f5c764820b49c9707f03f6afc5
Rahul Dhiman <rsdm34@gmail.com>
2024-04-08 11:50:07 -0700

    Removed unwanted .env test case (No .env file on github)

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
 	"os"
 	"testing"
 )
@@ -12,10 +11,6 @@ func TestRun(t *testing.T) {
 		t.Fatal("Failed to change directory:", err)
 	}
 
-	err = godotenv.Load()
-	if err != nil {
-		t.Fatal("Error loading .env file for testing:", err)
-	}
 	_, err = run()
 	if err != nil {
 		t.Error("Failed Run()")
